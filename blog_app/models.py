7d4338ec2ac7d9a1bb1feeeee71aa4949b82ed54
mungai hosea <mungaihosea@gmail.com>
2021-08-04 22:49:27 +0300

    initialized database and setup the api folder

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length= 30)
+    img = models.ImageField()
+    timestamp = models.DateTimeField(auto_now_add= True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+    
+    def __str__(self):
+        return self.title
